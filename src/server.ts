8f8da8a2776933341c273e59fd6a50bbc7a6fffa
CamilaMaturano00 <maturano4219@gmail.com>
2022-07-25 14:27:53 -0300

    Agrego LogIn y LogOut

--- src/server.ts
+++ src/server.ts
@@ -28,5 +28,5 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "..", "views"));
 
 app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+  console.log("Server is running at http://localhost:3000 ");
 });
